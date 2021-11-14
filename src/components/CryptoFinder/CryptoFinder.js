2fbcaee882df4372570a13f4028bb817aad7444b
ChristianKnopert <christianknopert@hotmail.com>
2021-11-14 22:26:23 +0100

    eindopdracht final

---
+++ src/components/CryptoFinder/CryptoFinder.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import './SearchBar.css';
+
+function SearchBar({ setLocationHandler }) {
+    const [query, setQuery] = useState('');
+
+    function handleClick() {
+        setLocationHandler(query);
+    }
+
+    function keyPressCheck(e) {
+        if (e.keyCode === 13) {
+            setLocationHandler(query);
+        }
+    }
+
+    return (
+        <span className="searchbar">
+      <input
+          type="text"
+          name="search"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          onKeyUp={keyPressCheck}
+          placeholder="Zoek een stad in Nederland"
+      />
+
+      <button
+          type="button"
+          onClick={handleClick}
+      >
+        Zoek
+      </button>
+    </span>
+    );
+}
+
+export default SearchBar;
